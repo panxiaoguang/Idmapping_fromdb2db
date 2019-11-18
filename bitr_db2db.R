aac5225f7b9656e6ab89410f63ad10dc148b7943
panxiaoguang <43256809+panxiaoguang@users.noreply.github.com>
2019-11-18 14:46:43 +0800

    Update bitr_db2db.R

--- bitr_db2db.R
+++ bitr_db2db.R
@@ -18,7 +18,7 @@ bitr_db2db<-function(method,input,inputvalues,output){
     response <- getForm(uri = "https://biodbnet-abcc.ncifcrf.gov/webServices/rest.php/biodbnetRestApi.json",method=method,format="row",input=input,inputValues=input_lst,outputs=output)
     result<-fromJSON(response)
     fin<-data.frame(from=inputvalues,to=sapply(result,function(x) x[[output]]))
-    if(grep("//",as.character(fin[,2]))){ ### if one 2 more
+    if(length(grep("//",as.character(fin[,2])))>0){ ### if one 2 more
       print("waring:your id have more than one mapping!")
       final<-separate_rows(fin,to,sep="//")
     }else{
