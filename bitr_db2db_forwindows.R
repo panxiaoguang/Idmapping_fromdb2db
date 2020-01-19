668c6709d82cacc92ea3160daad08ce16dfb75c5
panxiaoguang <43256809+panxiaoguang@users.noreply.github.com>
2020-01-19 11:05:15 +0000

    Update bitr_db2db_forwindows.R

--- bitr_db2db_forwindows.R
+++ bitr_db2db_forwindows.R
@@ -4,19 +4,19 @@ library(tidyr)
 ### main function
 bitr_db2db<-function(method,input,inputvalues,output){
   if(method=="getinputs"){
-    response<-GET("https://biodbnet-abcc.ncifcrf.gov/webServices/rest.php/biodbnetRestApi.json",query=list(method=method),accept("application/json"))
+    response<-GET("https://biodbnet-abcc.ncifcrf.gov/webServices/rest.php/biodbnetRestApi.json",query=list(method=method))
     json<-toJSON(content(response))
     result<-fromJSON(json)
     result$input
   }else if(method=="getoutputsforinput"){
-    response<-GET("https://biodbnet-abcc.ncifcrf.gov/webServices/rest.php/biodbnetRestApi.json",query=list(method=method,input=input),accept("application/json"))
+    response<-GET("https://biodbnet-abcc.ncifcrf.gov/webServices/rest.php/biodbnetRestApi.json",query=list(method=method,input=input))
     json<-toJSON(content(response))
     result<-fromJSON(json)
     result$output
   }else{
     method<-"db2db"
     input_lst<-paste(inputvalues,collapse=",")
-    response <- GET("https://biodbnet-abcc.ncifcrf.gov/webServices/rest.php/biodbnetRestApi.json",query=list(method=method,format="row",input=input,inputValues=input_lst,outputs=output),accept("application/json"))
+    response <- GET("https://biodbnet-abcc.ncifcrf.gov/webServices/rest.php/biodbnetRestApi.json",query=list(method=method,format="row",input=input,inputValues=input_lst,outputs=output))
     json<-toJSON(content(response))
     result<-fromJSON(json)
     fin<-data.frame(from=inputvalues,to=sapply(result,function(x) x[[output]]))
